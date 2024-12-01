644ddb02b7b29f665b0201ecca73158e857bd22c
b2ycompany <b2ylion@gmail.com>
2024-12-01 07:19:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-
+// Exemplo: Função de envio de formulário document.querySelector('form').addEventListener('submit', function (e) { e.preventDefault(); alert('Mensagem enviada! Entraremos em contato em breve.'); });

d1160f49b33c69d362af5109bd96696f1cfa582b
b2ycompany <b2ylion@gmail.com>
2024-12-01 08:02:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// Exemplo: Função de envio de formulário document.querySelector('form').addEventListener('submit', function (e) { e.preventDefault(); alert('Mensagem enviada! Entraremos em contato em breve.'); });
+// Exemplo: Função de envio de formulário 
+document.querySelector('form').addEventListener('submit', function (e) { e.preventDefault(); alert('Mensagem enviada! Entraremos em contato em breve.'); });
+// Função para adicionar produto ao carrinho 
+document.querySelectorAll('.add-to-cart').forEach(button => { button.addEventListener('click', () => { alert('Produto adicionado ao carrinho!'); }); });
